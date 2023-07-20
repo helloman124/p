9260fa4291a034df15c521bc8301340a5cfb8f5e
hellomoto76 <123373882+hellomoto76@users.noreply.github.com>
2023-07-21 00:25:42 +0530

    Update lph.py

--- lph.py
+++ lph.py
@@ -1,7 +1,7 @@
 import util
 celsius=float(input("enter temp in celsius:"))
 fahrenheit=util.celsius_to_fahrenheit(celsius)
-print("celsius in fahrenheit is:")
+print("celsius in fahrenheit is:",fahrenheit)
 fahrenheit=float(input("enter temp in fahrenheit:"))
 celsius=util.fahrenheit_to_celsius(fahrenheit)
-print("fahrenheit in celsius is:")
+print("fahrenheit in celsius is:",celsius)
