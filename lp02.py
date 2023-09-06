939db3c7842085d0b1afab85498a84968f432e07
hellomoto76 <123373882+hellomoto76@users.noreply.github.com>
2023-09-06 23:15:19 +0530

    Update lp02.py

--- lp02.py
+++ lp02.py
@@ -1,1 +1,15 @@
-gb
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+c=input("enter the operation symbol: ")
+if(c=='+'):
+    print(a+b)
+elif(c=='-'):
+    print(a-b)
+elif(c=='*'):
+    print(a*b)
+elif(c=='/'):
+    print(a/b)
+elif(c=='%'):
+    print(a%b)
+else:
+    print("invalid operation")
