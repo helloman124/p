13ade8171db1f2b18b6ab1909b68947f9757a2ea
hellomoto76 <123373882+hellomoto76@users.noreply.github.com>
2023-09-06 23:18:15 +0530

    Update lpg.py

--- lpg.py
+++ lpg.py
@@ -1,1 +1,20 @@
-f
+def length(string):
+    if string == "":
+        return 0
+    else:
+        return 1 + length(string[1:])
+print(length("python programming"))
+
+def smallest(lst):
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return min(lst[0], smallest(lst[1:]))
+print(smallest(['bg','a','rab']))
+def reverse(string):
+    b=""
+    if len(string)==0:
+        return string
+    else:
+        return string[-1]+reverse(string[:-1])
+print(reverse("hello man"))
